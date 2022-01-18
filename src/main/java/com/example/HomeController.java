de1b4fc1dbc6c7b3c363fc556d8949a8ca8e2b06
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-01-18 19:19:18 +0530

    sending data from controller to view

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,13 +1,18 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/home")
-	public String getHomePage()
+	public ModelAndView getHomePage()
 	{
-		return "home";
+		ModelAndView mv= new ModelAndView();
+		mv.setViewName("home");
+		mv.addObject("title","Home Page");
+		return mv;
 	}
 }
--- src/main/java/com/example/ProfileController.java
+++ src/main/java/com/example/ProfileController.java
@@ -17,3 +17,5 @@ public class ProfileController {
 		return "personalProfile";
 	}	
 }
+
+
